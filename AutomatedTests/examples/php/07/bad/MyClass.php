5697ab2f1c7be60fb8781b2077a4601fff781aa6
Rebel-L <dj@rebel-l.net>
2016-08-23 10:34:18 +0200

    added example for dependent tests and reorganized the example numbers

---
+++ AutomatedTests/examples/php/07/bad/MyClass.php
@@ -0,0 +1,15 @@
+<?php
+class MyClass
+{
+    private $value;
+
+    public function getValue()
+    {
+        // something goes wrong here
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+}
---
+++ AutomatedTests/examples/php/07/good/MyClassTest.php
@@ -0,0 +1,22 @@
+<?php
+require_once ('MyClass.php');
+
+class MyClassTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetValueHappy()
+    {
+        $my = new MyClass();
+        $this->assertSame('', $my->getValue(), 'Expects empty string');
+    }
+
+    /**
+     * @depends testGetValueHappy
+     */
+    public function testSetValueHappy()
+    {
+        $fixture = 'myValue';
+        $my = new MyClass();
+        $my->setValue($fixture);
+        $this->assertSame($fixture, $my->getValue());
+    }
+}
